fae987600f43f5392ea7b068172b45b299541253
Nilesh Tiwari <nileshtiwari2441996@gmail.com>
2023-02-21 00:31:00 +0530

    handled route not found

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const routes=require('./routes/api-v1-words');
+const notFound=require('./middleware/notFound.js')
 const connectDB=require('./db/connect')
 // require('./db/connect'); // db will be created after we added connections and the documnets
+
 app.use(express.static('./methods-public'));
 app.use(express.json());
 require('dotenv').config();
@@ -12,6 +14,7 @@ app.use('/api/v1/words',routes);
 app.get('/',(req,res)=>{
     app.send('./methods-public/index.html');
 })
+app.use(notFound);
 
 const port=5000;
 
